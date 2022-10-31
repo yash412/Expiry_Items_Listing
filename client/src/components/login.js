672d4e1bc15ba3fcb943e392498625d1d47d4726
Yash Choudhari <yashchoudhari.ysc@gmail.com>
2022-10-31 21:25:37 +0530

    added navbar done some visual changes

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,50 +1,87 @@
-import React , {useState , useContext } from 'react' ;
-import axios from 'axios' ;
-import { useNavigate } from 'react-router-dom' ;
-import { store } from '../App';
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { store } from "../App";
 
 const Login = () => {
-    const navigate = useNavigate() ;
-    // const [token,setToken] = useContext(store) ;
-    const [inputs , setInputs ] = useState({
-        email:"",
-        password:""
-    }) ;
-    const handleChange = (e) => {
-        setInputs({
-            ...inputs , [e.target.name] : e.target.value
-        })
+  const navigate = useNavigate();
+  // const [token,setToken] = useContext(store) ;
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const sendRequest = () => {
+    axios
+      .post("http://localhost:8000/api/user/login", {
+        email: inputs.email,
+        password: inputs.password,
+      })
+      .then((res) => localStorage.setItem("user", res.data));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendRequest();
+    if (localStorage.getItem("user")) {
+      navigate("/myprofile");
     }
-    const sendRequest = () => {
-        axios.post('http://localhost:8000/api/user/login',{email:inputs.email,password:inputs.password})
-        .then(res => localStorage.setItem('user',res.data)) ;
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        sendRequest() ;
-       if(localStorage.getItem('user')){
-          navigate('/myprofile') ;
-       }
-        // if(token){
-        //     navigate('/myprofile') ;
-        // }
-    }   
-    
-  return (
-    <div>
-    <h1>Login</h1>
-    <form onSubmit={handleSubmit}>
-        
-        <input type="text" placeholder="enter email" onChange={handleChange} name="email"
-        value={inputs.email}  required/><br/><br/>
-        <input type="text" placeholder="enter password" onChange={handleChange} name="password"
-        value={inputs.password} required/><br/><br/>
-        <button type="submit" disabled={
-            inputs.email && inputs.password.length > 5 ? false :true
-        }>Submit</button>
-    </form>
-</div>
-  )
-}
+    // if(token){
+    //     navigate('/myprofile') ;
+    // }
+  };
 
-export default Login ;
+  return (
+    <div className="auth-form-container m-2 p-5">
+      <div className="flex-container m-2 p-5">
+        <div>
+          <h1>Login</h1>
+        </div>
+        <div>
+          <form
+            className="login-form border border-success border-3 rounded m-2 p-5"
+            onSubmit={handleSubmit}
+          >
+            <div className="form-outline">
+              <label htmlFor="email"></label>
+              <input
+                type="text"
+                placeholder="Email Address"
+                onChange={handleChange}
+                name="email"
+                value={inputs.email}
+                required
+              />
+            </div>
+            <br />
+            <label htmlFor="password"></label>
+            <input
+              type="text"
+              placeholder="Password"
+              onChange={handleChange}
+              name="password"
+              value={inputs.password}
+              required
+            />
+            <br />
+            <br />
+            <button class="btn btn-success"
+              type="submit"
+              disabled={
+                inputs.email && inputs.password.length > 5 ? false : true
+              }
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,16 +1,34 @@
-import React from 'react' ;
-import {Link} from 'react-router-dom' ;
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-      <h1>
-        <Link to="/login" style={{marginLeft:"25px"}}>Login</Link>
-        <Link to="/register" style={{marginLeft:"25px"}}>Register</Link>
-        <Link to="/myprofile" style={{marginLeft:"25px"}}>Myprofile</Link>
-      </h1>
+      <>
+      <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" sty href="/Login">
+              Expiry Items Listing
+            </a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/Login">
+                  <button type="button" class="btn btn-success">Log in</button>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" href="/Register">
+                    <button type="button" class="btn btn-success">Sign up</button>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar ;
+export default Navbar;
